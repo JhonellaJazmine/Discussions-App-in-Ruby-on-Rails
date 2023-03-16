86b70bb738c16000d3d6a2ba9f03a42e1ba685ec
JhonellaJazmine <orquillojhonella@gmail.com>
2023-03-17 02:38:10 +0800

    Add discussions scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root to: "home#index"
+  resources :discussions
+  root to: "discussions#index"
   devise_for :users, controllers: {registrations: 'registrations'}
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_16_181740) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_16_182836) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "discussions", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,7 +34,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_16_181740) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "username", unique: true
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
